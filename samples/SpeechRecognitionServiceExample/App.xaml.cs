4b04de863459642e969a2b4937e41e00a4a73c96
Christian Spiecker <christian.spiecker@student.hs-rm.de>
2016-12-08 14:40:23 +0100

    Projektdateien hinzufügen.

---
+++ samples/SpeechRecognitionServiceExample/App.xaml.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="App.xaml.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+//
+// Microsoft Cognitive Services (formerly Project Oxford): https://www.microsoft.com/cognitive-services
+//
+// Microsoft Cognitive Services (formerly Project Oxford) GitHub:
+// https://github.com/Microsoft/Cognitive-Speech-STT-Windows
+//
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+namespace Microsoft.CognitiveServices.SpeechRecognition
+{
+    using System;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
+/*
+ * 
+ * Linguistic Analysis - Preview	5,000 transactions per month, 2 per second.	
+Key 1:
+cbb0747ff05a43638432926433537cf2
+Regenerate | Hide | Copy
+Key 2:
+6673fc64639b4e0baac93f709ebff6be
+Regenerate | Hide | Copy
+ * 
+ * 
+ * Bing Speech - Preview	5,000 transactions per month, 20 per minute for each feature for a total of 60 per minute.	
+Key 1:
+b943e5d1a1b5426f84720d15ad9be64e
+Regenerate | Hide | Copy
+Key 2:
+e381a6966aa24e73a767d097a7396908
+Regenerate | Hide | Copy
+ * 
+ * 
+ * */
